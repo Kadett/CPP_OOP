e79b660060a8a3d0a738e7243425bc569bca1fae
Evgen <Kadett0303@gmail.com>
2021-07-26 02:55:50 +0300

    add classes PassengerCar(), Bus(), Minivan();
    add Lesson3/Task2/PassengerCar.h
    add Lesson3/Task2/PassengerCar.cpp
    
    add Lesson3/Task2/Bus.h
    add Lesson3/Task2/Bus.cpp
    
    add Lesson3/Task2/Minivan.h
    add Lesson3/Task2/Minivan.cpp
    
    update Lesson3/CMakeLists.txt

---
+++ Lesson3/Task2/Bus.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Evgeniy Onishchenko on 026 26.07.21.
+//
+
+#include "Bus.h"
---
+++ Lesson3/Task2/Bus.h
@@ -0,0 +1,13 @@
+//
+// Created by Evgeniy Onishchenko on 026 26.07.21.
+//
+
+#pragma once
+
+class Bus {
+
+};
+
+
+
+
