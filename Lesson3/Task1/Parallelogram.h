1bd2ca8282df5426cebcddaf4558450bc8032317
Evgen <Kadett0303@gmail.com>
2021-07-26 01:53:16 +0300

    update Parallelogram.h;

--- Lesson3/Task1/Parallelogram.h
+++ Lesson3/Task1/Parallelogram.h
@@ -1,9 +1,12 @@
 #pragma once
+#include "Figure.h"
+class Parallelogram : public Figure{
+public:
+    double side_a, side_b;
 
-class Parallelogram {
+    Parallelogram(double side_a, double side_b);
 
+    explicit Parallelogram(double side_a);
+
+    double area() override = 0;
 };
-
-
-
-
