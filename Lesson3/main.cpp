1f7ff34296587ca39594fdc994354043cb4ee034
Evgen <Kadett0303@gmail.com>
2021-07-26 02:38:06 +0300

    completed the task 1
    update Lesson3/main.h
    update Lesson3/main.cpp

--- Lesson3/main.cpp
+++ Lesson3/main.cpp
@@ -1,7 +1,17 @@
 #include "main.h"
 
 int main(){
-    
+    // Task 1
+    std::cout << "Task 1:" << std::endl;
+    Circle circle(15);
+    Rhombus rhombus(13, 5);
+    Square square(25);
+    Rectangle rectangle(10, 8);
+    std::cout << "The area of the circle = " << circle.area() << std::endl;
+    std::cout << "The area of the rhombus = " << rhombus.area() << std::endl;
+    std::cout << "The area of the square = " << square.area() << std::endl;
+    std::cout << "The area of the rectangle = " << rectangle.area() << std::endl;
+    std::cout << "\n\n";
 
     return 0;
 }
--- Lesson3/main.h
+++ Lesson3/main.h
@@ -1,2 +1,6 @@
 #pragma once
 #include <iostream>
+#include "Task1/Rectangle.h"
+#include "Task1/Circle.h"
+#include "Task1/Rhombus.h"
+#include "Task1/Square.h"
