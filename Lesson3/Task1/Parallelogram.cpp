0972f433eaf82fa75b1ae51eca33a7fe71a434f1
Evgen <Kadett0303@gmail.com>
2021-07-26 01:54:52 +0300

    completed the abstract class Parallelogram;
    update  Parallelogram.cpp

--- Lesson3/Task1/Parallelogram.cpp
+++ Lesson3/Task1/Parallelogram.cpp
@@ -1,5 +1,10 @@
 //
-// Created by Evgeniy Onishchenko on 026 26.07.21.
+// Created by User on 022 22.07.21.
 //
 
 #include "Parallelogram.h"
+
+Parallelogram::Parallelogram(double side_a, double side_b) : side_a(side_a), side_b(side_b) {}
+
+Parallelogram::Parallelogram(double side_a) : Parallelogram(side_a, side_a) {}
+
