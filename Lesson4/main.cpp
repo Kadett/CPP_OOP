705d0441198e17a3ee30701b433d6abdfba767fe
Evgen <Kadett0303@gmail.com>
2021-07-26 04:48:14 +0300

    add functions initVectorRandom(), countUniqueNumbers();

--- Lesson4/main.cpp
+++ Lesson4/main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
 #include "Task1/ArrayInt.h"
+#include <vector>
+#include <set>
+#include <ctime>
+
+
+void initVectorRandom(std::vector<int> &v, uint32_t max_number, size_t size) {
+    srand(time(0));
+    v.clear();
+    v.reserve(size);
+    for (int i = 0; i < size; ++i) {
+        v.push_back(rand() % max_number);
+    }
+}
+
+int countUniqueNumbers(std::vector<int> &vector){
+    std::set<int> s;
+    for(auto &i : vector){
+        s.insert(i);
+    }
+    return s.size();
+}
+
 
 
 int main(){
