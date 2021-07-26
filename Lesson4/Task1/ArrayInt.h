f334295f86c779fd556c78f37e63ed3f8d13b2a2
Evgen <Kadett0303@gmail.com>
2021-07-26 04:23:45 +0300

    completed class ArrayInt();
    update Lesson/Task1/ArrayInt.h
    update Lesson/Task1/ArrayInt.cpp

--- Lesson4/Task1/ArrayInt.h
+++ Lesson4/Task1/ArrayInt.h
@@ -1,13 +1,26 @@
-//
-// Created by Evgeniy Onishchenko on 026 26.07.21.
-//
-
 #pragma once
+#include <iostream>
+#include <cassert>
+#include <algorithm>
 
 class ArrayInt {
+    int m_length;
+    int *m_data;
+public:
+    ArrayInt();
+    ArrayInt(int length);
+    ~ArrayInt();
 
+    void erase();
+    int getLength();
+    int& operator[](int index);
+
+    void resize(int newLength);
+
+    void insertBefore(int value, int index);
+    void push_back(int value);
+    void pop_back();
+    void pop_front();
+    void sort();
+    void print();
 };
-
-
-
-
