8c6589b427e46df88c692d32c99be8b84228a5d8
Evgen <Kadett0303@gmail.com>
2021-07-28 00:47:19 +0300

    complete task 1
    update Lesson5/main.cpp
    update Lesson5/src/Pair.h

--- Lesson5/main.cpp
+++ Lesson5/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "src/Pair1.h"
 
+int main() {
+    // Task 1
+    Pair1<int> p1(6, 9);
+    std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
 
-int main(){
+    const Pair1<double> p2(3.4, 7.8);
+    std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
 
+    
 
     return 0;
 }
--- Lesson5/src/Pair1.h
+++ Lesson5/src/Pair1.h
@@ -1,6 +1,12 @@
 #pragma once
 
 template<typename T>
-class Pair1{
-    
+class Pair1 {
+    T _first, _second;
+public:
+    Pair1(const T &first, const T &second) : _first(first), _second(second) {}
+
+    T first() const { return _first; }
+
+    T second() const { return _second; }
 };
