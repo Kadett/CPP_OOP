7109b3679407e20736113631368c7defee5bc422
Evgen <Kadett0303@gmail.com>
2021-07-28 01:13:48 +0300

    complete Task 2
    update Lesson5/src/Pair.h
    update Lesson5/main.cpp

--- Lesson5/main.cpp
+++ Lesson5/main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include "src/Pair1.h"
+#include "src/Pair.h"
 
 int main() {
     // Task 1
-    Pair1<int> p1(6, 9);
-    std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+    {
+        Pair1<int> p1(6, 9);
+        std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
 
-    const Pair1<double> p2(3.4, 7.8);
-    std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+        const Pair1<double> p2(3.4, 7.8);
+        std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+    }
+    // Task 2
+    {
+        Pair<int, double> p1(6, 7.8);
+        std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
 
+        const Pair<double, int> p2(3.4, 5);
+        std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+    }
 
 
     return 0;
--- Lesson5/src/Pair.h
+++ Lesson5/src/Pair.h
@@ -1,2 +1,12 @@
 #pragma once
 
+template<typename T1, typename T2>
+class Pair {
+    T1 _first;
+    T2 _second;
+public:
+    Pair(T1 first, T2 second) : _first(first), _second(second) {}
+
+    T1 first() const { return _first; }
+    T2 second() const { return _second; }
+};
