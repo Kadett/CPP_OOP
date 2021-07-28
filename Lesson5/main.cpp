422cda06a9f3ef9dfe68e56fd3b9e750bf25c387
Evgen <Kadett0303@gmail.com>
2021-07-28 10:00:02 +0300

    complete task 3
    create Lesson5/src/StringValuePair.h
    update: Lesson5/src/Pair.h,  Lesson5/src/StringValuePair.h, Lesson5/CMakeLists.txt, Lesson5/main.cpp

--- Lesson5/main.cpp
+++ Lesson5/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "src/Pair1.h"
 #include "src/Pair.h"
+#include "src/StringValuePair.h"
+
 
 int main() {
     // Task 1
@@ -13,11 +15,18 @@ int main() {
     }
     // Task 2
     {
+        std::cout << std::endl;
         Pair<int, double> p1(6, 7.8);
         std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
 
         const Pair<double, int> p2(3.4, 5);
         std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+    }
+
+    {
+        std::cout << std::endl;
+        StringValuePair<int> svp("Amazing", 7);
+        std::cout << "Pair: " << svp.first() << ' ' << svp.second() << '\n';
     }
 
 
--- Lesson5/src/Pair.h
+++ Lesson5/src/Pair.h
@@ -1,12 +1,15 @@
 #pragma once
 
+
 template<typename T1, typename T2>
 class Pair {
     T1 _first;
     T2 _second;
 public:
-    Pair(T1 first, T2 second) : _first(first), _second(second) {}
+    Pair(const T1& first, const T2& second) : _first(first), _second(second) {}
 
-    T1 first() const { return _first; }
-    T2 second() const { return _second; }
+    const T1& first() const { return _first; }
+
+    const T2& second() const { return _second; }
 };
+
