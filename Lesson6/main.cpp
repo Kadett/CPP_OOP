ad6709a9970a022c21788f47e7821c4a58270703
Evgen <Kadett0303@gmail.com>
2021-08-01 02:02:55 +0300

    Completed Task 1
    update Lesson6/main.cpp

--- Lesson6/main.cpp
+++ Lesson6/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <limits>
+
+// Task 1
+void getInt(){
+    int num;
+    while (true){
+        std::cout << "Enter a number:" << std::endl;
+        std::cin >> num;
+        if (std::cin.fail()){
+            std::cout << "Error: not a number!" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        if (std::cin.gcount() > 1) {
+            std::cout << "Error: not a number!" << std::endl;
+            continue;
+        }
+        break;
+    }
+}
 
 int main(){
-
-
+    // Task 1
+    getInt();
 
     return 0;
 }
