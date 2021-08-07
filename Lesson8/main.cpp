f8897368b7e95ebe1117a0b3c39fa174bc3b0a6b
Evgen <Kadett0303@gmail.com>
2021-08-07 03:06:30 +0300

    add function div()
    update Lesson8/main.cpp
    completed function div()

--- Lesson8/main.cpp
+++ Lesson8/main.cpp
@@ -1,4 +1,19 @@
-//
-// Created by Evgeniy Onishchenko on 007 07.08.21.
-//
+#include <iostream>
+// Task 1
+template<typename T>
+void div(T numerator, T denominator){
+    if (denominator == 0) throw std::runtime_error("Division by zero");
+    std::cout << numerator << "/" << denominator << " = " << static_cast<double>(numerator)/denominator << std::endl;
+}
 
+int main(){
+    // Task 1
+    try {
+        div<int>(10, 3);
+        div<int>(5, 0);
+    } catch (std::runtime_error &e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
